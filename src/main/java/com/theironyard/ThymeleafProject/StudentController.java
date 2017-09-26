cc200a2c8876c061270ccc3d5434357bebaff886
hamilton moore <hamlmoor@gmail.com>
2017-09-26 18:22:36 -0500

    first commit

---
+++ src/main/java/com/theironyard/ThymeleafProject/StudentController.java
@@ -0,0 +1,46 @@
+package com.theironyard.ThymeleafProject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class StudentController {
+    Student student;
+
+
+    @RequestMapping("view_student")
+    public String viewStudent(Model model) {
+        model.addAttribute("student", student);
+        return "view_student";
+    }
+
+    @RequestMapping("/new_student")
+    public String newStudent(Model model) {
+        System.out.println(Grade.values());
+        model.addAttribute("grades", Grade.values());
+
+        return "new_student";
+    }
+
+    @RequestMapping(path = "/create_student", method = RequestMethod.POST)
+    public String createStudent(@RequestParam(value="first_name") String firstName,
+                                @RequestParam(value="last_name") String lastName,
+                                @RequestParam(value="grade") Grade grade) {
+
+        Student student = new Student();
+
+        /* set student firstName, lastName and grade using the http request parameters */
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setGrade(grade);
+        this.student = student;
+//
+//        /* add the student to the model that will be used by the view_student html file */
+//        model.addAttribute("Student", student);
+
+        return "redirect:/view_student.html";
+    }
+}
